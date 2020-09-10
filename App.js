bae701d6fee473060319264c428b66db1e40adfe
Alexander Cabezas <alexander.cabezas@autodesk.com>
2020-09-10 20:08:09 -0300

    cambios para demostrar como funciona git

--- App.js
+++ App.js
@@ -8,22 +8,22 @@
  * Licensed under MIT (https://github.com/creativetimofficial/material-kit-react-native/blob/master/LICENSE)
  =========================================================
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+blabla
 */
 
-import React from 'react';
-import { Platform, StatusBar, Image } from 'react-native';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import { Block, GalioProvider } from 'galio-framework';
+import React from "react";
+import { Platform, StatusBar, Image } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import { Block, GalioProvider } from "galio-framework";
 
-import { Images, products, materialTheme } from './constants/';
+import { Images, products, materialTheme } from "./constants/";
 
-import { NavigationContainer } from '@react-navigation/native';
-import Screens from './navigation/Screens';
+import { NavigationContainer } from "@react-navigation/native";
+import Screens from "./navigation/Screens";
 
 // Before rendering any navigation stack
-import { enableScreens } from 'react-native-screens';
+import { enableScreens } from "react-native-screens";
 enableScreens();
 
 // cache app images
@@ -35,11 +35,11 @@ const assetImages = [
 ];
 
 // cache product images
-products.map(product => assetImages.push(product.image));
+products.map((product) => assetImages.push(product.image));
 
 function cacheImages(images) {
-  return images.map(image => {
-    if (typeof image === 'string') {
+  return images.map((image) => {
+    if (typeof image === "string") {
       return Image.prefetch(image);
     } else {
       return Asset.fromModule(image).downloadAsync();
@@ -66,7 +66,7 @@ export default class App extends React.Component {
         <NavigationContainer>
           <GalioProvider theme={materialTheme}>
             <Block flex>
-              {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+              {Platform.OS === "ios" && <StatusBar barStyle="default" />}
               <Screens />
             </Block>
           </GalioProvider>
@@ -76,12 +76,10 @@ export default class App extends React.Component {
   }
 
   _loadResourcesAsync = async () => {
-    return Promise.all([
-      ...cacheImages(assetImages),
-    ]);
+    return Promise.all([...cacheImages(assetImages)]);
   };
 
-  _handleLoadingError = error => {
+  _handleLoadingError = (error) => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
     console.warn(error);
