ba898b44e5282c4644cd5cd21f9ffa6abdf7603b
Alexander Cabezas <alexander.cabezas@autodesk.com>
2020-09-12 23:09:23 -0300

    Implementación de Lista de Ingresos

--- screens/Ingresos.js
+++ screens/Ingresos.js
@@ -1,256 +1,228 @@
-import React, { Component } from "react";
-import { StyleSheet, Dimensions, Alert } from "react-native";
-import * as Yup from "yup";
-import { Formik } from "formik";
-import { Block, theme } from "galio-framework";
-import { materialTheme } from "../constants/";
-
+import React, { useState } from "react";
 import {
-  Container,
-  Header,
-  Content,
-  Footer,
-  Form,
-  Item,
-  Input,
-  Label,
-  Picker,
-  Icon,
-  DatePicker,
-  Button,
+  StyleSheet,
   Text,
-} from "native-base";
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { SwipeListView } from "react-native-swipe-list-view";
 
-const { width } = Dimensions.get("screen");
+export default function ListaIngresos() {
+  const [listData, setListData] = useState([
+    {
+      title: "Mensual",
+      data: [
+        {
+          key: 1,
+          cantidad: 145000000.5,
+          origen: "Facturación como Autónomo",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+        {
+          key: 2,
+          cantidad: 32000.0,
+          origen: "Alquiler de Propiedad",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+        {
+          key: 3,
+          cantidad: 900000,
+          origen: "Sueldo en Relacion de Dependencia",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+      ],
+    },
+    {
+      title: "Semanal",
+      data: [
+        {
+          key: 4,
+          cantidad: 145000000.5,
+          origen: "Sueldo en Relacion de Dependencia",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+        {
+          key: 5,
+          cantidad: 32000.0,
+          origen: "Sueldo en Relacion de Dependencia",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+      ],
+    },
+    {
+      title: "Diario",
+      data: [
+        {
+          key: 4,
+          cantidad: 5000,
+          origen: "Extraordinario",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+        {
+          key: 5,
+          cantidad: 4000.0,
+          origen: "Otros",
+          destino: "HSBC 85945849584989",
+          recibido_en: "03/04/2020",
+          creado_en: "03/04/2020",
+        },
+      ],
+    },
+  ]);
 
-const thumbMeasure = (width - 48 - 32) / 3;
+  const B = (props) => (
+    <Text style={{ fontWeight: "bold" }}>{props.children}</Text>
+  );
 
-export default class RegistrarIngreso extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { chosenDate: new Date() };
-    this.setDate = this.setDate.bind(this);
-  }
-  setDate(newDate) {
-    this.setState({ chosenDate: newDate });
-  }
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Text style={styles.title}>REGISTRAR INGRESO</Text>
-        </Header>
-        <Content>
-          <Form>
-            <Text style={styles.space}></Text>
-            <Item>
-              <Label>Fecha</Label>
-              <DatePicker
-                defaultDate={new Date(2018, 4, 4)}
-                minimumDate={new Date(2000, 1, 1)}
-                maximumDate={new Date(2100, 12, 31)}
-                locale={"es"}
-                timeZoneOffsetInMinutes={undefined}
-                modalTransparent={false}
-                animationType={"fade"}
-                androidMode={"default"}
-                placeHolderText="Elegir fecha"
-                textStyle={{ color: "green" }}
-                placeHolderTextStyle={{ color: "#d3d3d3" }}
-                onDateChange={this.setDate}
-                disabled={false}
-              />
-            </Item>
-            <Item stackedLabel>
-              <Label>Cantidad</Label>
-              <Input />
-            </Item>
-            <Item>
-              <Label>Frecuencia del Ingreso</Label>
-              <Picker
-                mode="dropdown"
-                iosIcon={<Icon name="arrow-down" />}
-                style={{ width: undefined }}
-                placeholder="Frecuencia del Ingreso"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                placeholderIconColor="#007aff"
-              >
-                <Picker.Item label="Mensual" value="mensual" />
-                <Picker.Item label="Semanal" value="semanal" />
-                <Picker.Item label="Diario" value="diario" />
-                <Picker.Item label="Una sola vez" value="onetime" />
-              </Picker>
-            </Item>
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  };
 
-            <Item>
-              <Label>Origen</Label>
-              <Picker
-                mode="dropdown"
-                iosIcon={<Icon name="arrow-down" />}
-                style={{ width: undefined }}
-                placeholder="Origen del Ingreso"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                placeholderIconColor="#007aff"
-              >
-                <Picker.Item
-                  label="Sueldo en Relación de Dependencia"
-                  value="sueldo_relacion_dependencia"
-                />
-                <Picker.Item
-                  label="Alquiler de Propiedad"
-                  value="alquiler_propiedad"
-                />
-                <Picker.Item
-                  label="Facturación como Autónomo"
-                  value="facturacion_autonomo"
-                />
-                <Picker.Item label="Extraordinario" value="extraordinario" />
-                <Picker.Item label="Otros" value="otros" />
-              </Picker>
-            </Item>
-            <Item>
-              <Label>Destino</Label>
-              <Picker
-                mode="dropdown"
-                iosIcon={<Icon name="arrow-down" />}
-                style={{ width: undefined }}
-                placeholder="Origen del Ingreso"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                placeholderIconColor="#007aff"
-              >
-                <Picker.Item
-                  label="Cuenta Bancaria #1"
-                  value="cuenta_bancaria_1"
-                />
-                <Picker.Item
-                  label="Cuenta Bancaria #2"
-                  value="cuenta_bancaria_2"
-                />
-                <Picker.Item
-                  label="Cuenta Bancaria #3"
-                  value="cuenta_bancaria_3"
-                />
-                <Picker.Item label="Efectivo" value="efectivo" />
-              </Picker>
-            </Item>
-            <Text style={styles.space}></Text>
-            <Button
-              block
-              primary
-              onPress={() => Alert.alert("Guardar clicked")}
-            >
-              <Text>Guardar</Text>
-            </Button>
-            <Button
-              block
-              bordered
-              light
-              onPress={() => Alert.alert("Cancelar clicked")}
-            >
-              <Text>Cancelar</Text>
-            </Button>
-          </Form>
-        </Content>
-        <Footer />
-      </Container>
-    );
-  }
+  const deleteRow = (rowMap, rowKey) => {
+    closeRow(rowMap, rowKey);
+    const newData = [...listData];
+    const prevIndex = listData.findIndex((item) => item.key === rowKey);
+    newData.splice(prevIndex, 1);
+    setListData(newData);
+  };
+
+  const onRowDidOpen = (rowKey) => {
+    console.log("This row opened", rowKey);
+  };
+
+  const renderItem = (data) => (
+    <TouchableHighlight style={styles.rowFront} underlayColor={"#AAA"}>
+      <View style={styles.item}>
+        <View style={{ width: 150 }}>
+          <Text style={{ paddingBottom: 5 }}>
+            <Text>ARS </Text>
+            <B>{data.item.cantidad}</B>
+          </Text>
+          <Text>Recibido el: {data.item.recibido_en}</Text>
+        </View>
+        <View style={{ width: 250 }}>
+          <B>Origen:</B>
+          <Text>{data.item.origen}</Text>
+          <B>Destino:</B>
+          <Text>{data.item.destino}</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+
+  const renderHiddenItem = (data, rowMap) => (
+    <View style={styles.rowBack}>
+      <View>
+        <Text>Registrado el:</Text>
+        <Text>03/05/2020</Text>
+      </View>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnLeft]}
+        onPress={() => closeRow(rowMap, data.item.key)}
+      >
+        <Icon color="white" size={30} name="keyboard-backspace" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnRight]}
+        onPress={() => deleteRow(rowMap, data.item.key)}
+      >
+        <Icon color="white" size={30} name="trash-can-outline" />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderSectionHeader = ({ section }) => (
+    <Text style={styles.sectionTitle}>{section.title}</Text>
+  );
+
+  return (
+    <View style={styles.container}>
+      <SwipeListView
+        useSectionList
+        sections={listData}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        renderSectionHeader={renderSectionHeader}
+        leftOpenValue={75}
+        rightOpenValue={-150}
+        previewRowKey={"0"}
+        previewOpenValue={-40}
+        previewOpenDelay={3000}
+        onRowDidOpen={onRowDidOpen}
+        minHeight={100}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  components: {},
-  title: {
-    paddingVertical: theme.SIZES.BASE,
-    paddingHorizontal: theme.SIZES.BASE * 2,
-    color: theme.COLORS.WHITE,
-    fontWeight: "bold",
+  container: {
+    backgroundColor: "white",
+    flex: 1,
   },
-  group: {
-    paddingTop: theme.SIZES.BASE * 3.75,
+  backTextWhite: {
+    color: "#FFF",
   },
-  shadow: {
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 4,
-    shadowOpacity: 0.2,
-    elevation: 2,
-  },
-  button: {
-    marginBottom: theme.SIZES.BASE,
-    width: width - theme.SIZES.BASE * 2,
-  },
-  optionsText: {
-    fontSize: theme.SIZES.BASE * 0.75,
-    color: "#4A4A4A",
-    fontWeight: "normal",
-    fontStyle: "normal",
-    letterSpacing: -0.29,
-  },
-  optionsButton: {
-    width: "auto",
-    height: 34,
-    paddingHorizontal: theme.SIZES.BASE,
-    paddingVertical: 10,
-  },
-  input: {
+  rowFront: {
+    alignItems: "flex-start",
+    backgroundColor: "white",
+    borderBottomColor: "#4f73f2",
     borderBottomWidth: 1,
-  },
-  inputDefault: {
-    borderBottomColor: materialTheme.COLORS.PLACEHOLDER,
-  },
-  inputTheme: {
-    borderBottomColor: materialTheme.COLORS.PRIMARY,
-  },
-  inputTheme: {
-    borderBottomColor: materialTheme.COLORS.PRIMARY,
-  },
-  inputInfo: {
-    borderBottomColor: materialTheme.COLORS.INFO,
-  },
-  inputSuccess: {
-    borderBottomColor: materialTheme.COLORS.SUCCESS,
-  },
-  inputWarning: {
-    borderBottomColor: materialTheme.COLORS.WARNING,
-  },
-  inputDanger: {
-    borderBottomColor: materialTheme.COLORS.ERROR,
-  },
-  imageBlock: {
-    overflow: "hidden",
-    borderRadius: 4,
-  },
-  rows: {
-    height: theme.SIZES.BASE * 2,
-  },
-  social: {
-    width: theme.SIZES.BASE * 3.5,
-    height: theme.SIZES.BASE * 3.5,
-    borderRadius: theme.SIZES.BASE * 1.75,
     justifyContent: "center",
+    height: 100,
+    paddingLeft: 15,
+    paddingRight: 15,
   },
-  category: {
-    backgroundColor: theme.COLORS.WHITE,
-    marginVertical: theme.SIZES.BASE / 2,
-    borderWidth: 0,
-  },
-  categoryTitle: {
-    height: "100%",
-    paddingHorizontal: theme.SIZES.BASE,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    justifyContent: "center",
+  rowBack: {
     alignItems: "center",
+    backgroundColor: "#DDD",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 15,
   },
-  albumThumb: {
-    borderRadius: 4,
-    marginVertical: 4,
-    alignSelf: "center",
-    width: thumbMeasure,
-    height: thumbMeasure,
+  backRightBtn: {
+    alignItems: "center",
+    bottom: 0,
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    width: 75,
   },
-  space: {
-    color: "#C0C0C0",
-    fontSize: 15,
-    textAlign: "justify",
+  backRightBtnLeft: {
+    backgroundColor: "#4f73f2",
+    right: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: "#d84343",
+    right: 0,
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  sectionTitle: {
+    fontSize: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 5,
+    borderBottomColor: "#4f73f2",
+    borderBottomWidth: 1,
   },
 });
---
+++ screens/IngresosRegistrar.js
@@ -0,0 +1,243 @@
+import React, { useState } from "react";
+import { StyleSheet, Dimensions, Alert } from "react-native";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { Block, theme } from "galio-framework";
+import { materialTheme } from "../constants";
+
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  Form,
+  Item,
+  Input,
+  Label,
+  Picker,
+  Icon,
+  DatePicker,
+  Button,
+  Text,
+} from "native-base";
+
+const { width } = Dimensions.get("screen");
+
+const thumbMeasure = (width - 48 - 32) / 3;
+
+export default function RegistrarIngreso() {
+  const [chosenDate, setDate] = useState(new Date());
+
+  return (
+    <Container>
+      <Header>
+        <Text style={styles.title}>REGISTRAR INGRESO</Text>
+      </Header>
+      <Content>
+        <Form>
+          <Text style={styles.space}></Text>
+          <Item>
+            <Label>Recibido el</Label>
+            <DatePicker
+              defaultDate={new Date(2018, 4, 4)}
+              minimumDate={new Date(2000, 1, 1)}
+              maximumDate={new Date(2100, 12, 31)}
+              locale={"es"}
+              timeZoneOffsetInMinutes={undefined}
+              modalTransparent={false}
+              animationType={"fade"}
+              androidMode={"default"}
+              placeHolderText="Elegir fecha"
+              textStyle={{ color: "green" }}
+              placeHolderTextStyle={{ color: "#d3d3d3" }}
+              onDateChange={setDate}
+              disabled={false}
+            />
+          </Item>
+          <Item stackedLabel>
+            <Label>Cantidad</Label>
+            <Input />
+          </Item>
+          <Item>
+            <Label>Frecuencia del Ingreso</Label>
+            <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              style={{ width: undefined }}
+              placeholder="Frecuencia del Ingreso"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+            >
+              <Picker.Item label="Mensual" value="mensual" />
+              <Picker.Item label="Semanal" value="semanal" />
+              <Picker.Item label="Diario" value="diario" />
+              <Picker.Item label="Una sola vez" value="onetime" />
+            </Picker>
+          </Item>
+          <Item>
+            <Label>Origen</Label>
+            <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              style={{ width: undefined }}
+              placeholder="Origen del Ingreso"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+            >
+              <Picker.Item
+                label="Sueldo en Relación de Dependencia"
+                value="sueldo_relacion_dependencia"
+              />
+              <Picker.Item
+                label="Alquiler de Propiedad"
+                value="alquiler_propiedad"
+              />
+              <Picker.Item
+                label="Facturación como Autónomo"
+                value="facturacion_autonomo"
+              />
+              <Picker.Item label="Extraordinario" value="extraordinario" />
+              <Picker.Item label="Otros" value="otros" />
+            </Picker>
+          </Item>
+          <Item>
+            <Label>Destino</Label>
+            <Picker
+              mode="dropdown"
+              iosIcon={<Icon name="arrow-down" />}
+              style={{ width: undefined }}
+              placeholder="Origen del Ingreso"
+              placeholderStyle={{ color: "#bfc6ea" }}
+              placeholderIconColor="#007aff"
+            >
+              <Picker.Item
+                label="Cuenta Bancaria #1"
+                value="cuenta_bancaria_1"
+              />
+              <Picker.Item
+                label="Cuenta Bancaria #2"
+                value="cuenta_bancaria_2"
+              />
+              <Picker.Item
+                label="Cuenta Bancaria #3"
+                value="cuenta_bancaria_3"
+              />
+              <Picker.Item label="Efectivo" value="efectivo" />
+            </Picker>
+          </Item>
+          <Text style={styles.space}></Text>
+          <Button block primary onPress={() => Alert.alert("Guardar clicked")}>
+            <Text>Guardar</Text>
+          </Button>
+          <Button
+            block
+            bordered
+            light
+            onPress={() => Alert.alert("Cancelar clicked")}
+          >
+            <Text>Cancelar</Text>
+          </Button>
+        </Form>
+      </Content>
+      <Footer />
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  components: {},
+  title: {
+    paddingVertical: theme.SIZES.BASE,
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    color: theme.COLORS.WHITE,
+    fontWeight: "bold",
+  },
+  group: {
+    paddingTop: theme.SIZES.BASE * 3.75,
+  },
+  shadow: {
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    shadowOpacity: 0.2,
+    elevation: 2,
+  },
+  button: {
+    marginBottom: theme.SIZES.BASE,
+    width: width - theme.SIZES.BASE * 2,
+  },
+  optionsText: {
+    fontSize: theme.SIZES.BASE * 0.75,
+    color: "#4A4A4A",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    letterSpacing: -0.29,
+  },
+  optionsButton: {
+    width: "auto",
+    height: 34,
+    paddingHorizontal: theme.SIZES.BASE,
+    paddingVertical: 10,
+  },
+  input: {
+    borderBottomWidth: 1,
+  },
+  inputDefault: {
+    borderBottomColor: materialTheme.COLORS.PLACEHOLDER,
+  },
+  inputTheme: {
+    borderBottomColor: materialTheme.COLORS.PRIMARY,
+  },
+  inputTheme: {
+    borderBottomColor: materialTheme.COLORS.PRIMARY,
+  },
+  inputInfo: {
+    borderBottomColor: materialTheme.COLORS.INFO,
+  },
+  inputSuccess: {
+    borderBottomColor: materialTheme.COLORS.SUCCESS,
+  },
+  inputWarning: {
+    borderBottomColor: materialTheme.COLORS.WARNING,
+  },
+  inputDanger: {
+    borderBottomColor: materialTheme.COLORS.ERROR,
+  },
+  imageBlock: {
+    overflow: "hidden",
+    borderRadius: 4,
+  },
+  rows: {
+    height: theme.SIZES.BASE * 2,
+  },
+  social: {
+    width: theme.SIZES.BASE * 3.5,
+    height: theme.SIZES.BASE * 3.5,
+    borderRadius: theme.SIZES.BASE * 1.75,
+    justifyContent: "center",
+  },
+  category: {
+    backgroundColor: theme.COLORS.WHITE,
+    marginVertical: theme.SIZES.BASE / 2,
+    borderWidth: 0,
+  },
+  categoryTitle: {
+    height: "100%",
+    paddingHorizontal: theme.SIZES.BASE,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  albumThumb: {
+    borderRadius: 4,
+    marginVertical: 4,
+    alignSelf: "center",
+    width: thumbMeasure,
+    height: thumbMeasure,
+  },
+  space: {
+    color: "#C0C0C0",
+    fontSize: 15,
+    textAlign: "justify",
+  },
+});
